de3cd50a5ac612c96138ae2f68f8081128efa7cc
Chris McKnight <cmckni3@gmail.com>
2015-12-18 01:31:32 -0600

    Initial commit

---
+++ dns.js
@@ -0,0 +1,32 @@
+var dns = require('native-dns'),
+  util = require('util');
+
+var question = dns.Question({
+  name: 'battlekittensoftware.info',
+  type: 'A',
+});
+
+var start = new Date().getTime();
+
+var req = dns.Request({
+  question: question,
+  server: { address: '8.8.8.8', port: 53, type: 'udp' },
+  timeout: 1000,
+});
+
+req.on('timeout', function () {
+  console.log('Timeout in making request');
+});
+
+req.on('message', function (err, answer) {
+  answer.answer.forEach(function (a) {
+    console.log(a.address);
+  });
+});
+
+req.on('end', function () {
+  var delta = (new Date().getTime()) - start;
+  console.log('Finished processing request: ' + delta.toString() + 'ms');
+});
+
+req.send();
